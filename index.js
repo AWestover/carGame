63bde30f74b0637ecfd9574a907a778c931b973b
awestover <alek.westover@gmail.com>
2017-11-13 17:16:23 -0500

    Trying to render to same screen

--- index.js
+++ index.js
@@ -19,12 +19,14 @@ io.sockets.on('connection', newConnection);
 
 function newConnection(socket) {
 	//console.log(socket.id);
-	socket.on('mouse', mouseMsg);
+	//socket.on('mouse', mouseMsg);
 	socket.on('key', keyMsg);
+	/*
 	function mouseMsg(data) {
 		socket.broadcast.emit('mouse', data);
 		//console.log(data);
 	}
+	*/
 	function keyMsg(key_data) {
 		socket.broadcast.emit('key', key_data);
 	}
--- public/sketch.js
+++ public/sketch.js
@@ -7,7 +7,8 @@ var screen_color = [0, 0, 0];
 
 var picture;
 
-var imageLoc = [300, 300];
+var playerLoc = [screen_dims[0]/2, screen_dims[1]/2];
+var enemyLoc = [screen_dims[0]/2, screen_dims[1]/2];
 
 function setup() {
 	createCanvas(screen_dims[0], screen_dims[1]);
@@ -16,61 +17,78 @@ function setup() {
 
 	socket = io.connect();
 
-	socket.on('mouse', newDrawing);
-	socket.on('key', displayText);
+	//socket.on('mouse', newDrawing);
+	socket.on('key', updateEnemy);
 }
 
+
+function draw() {
+	background(0, 0, 0);
+	displayCar(enemyLoc[0], enemyLoc[1]);
+	displayCar(playerLoc[0], playerLoc[1]);
+}
+
+function displayCar(x, y)
+{
+	imageMode(CENTER);
+	fill(255, 255, 255);
+	image(picture, x, y);
+}
+
+function keyReleased() {
+	var cUpdate = updateCar(key);
+	playerLoc[0] += cUpdate[0];
+	playerLoc[1] += cUpdate[1];
+	var key_data = {
+		k: key
+	}
+	socket.emit('key', key_data);
+	//displayText(key_data);
+}
+
+function updateCarVector(key)
+{
+	var speed = 10;
+	var outVector = [0, 0];
+	if (key == 'A' || key == 'a')
+	{
+		outVector[0] = -speed;
+	}
+	else if (key == 'd' || key == 'D')
+	{
+		outVector[0] = speed;
+	}
+	else if (key == 'w' || key == 'W')
+	{
+		outVector[1] = -speed;
+	}
+	else if (key == 's' || key == 'S')
+	{
+		outVector[1] = speed;
+	}
+	return outVector;
+}
+
+
+function updateEnemy(key_data)
+{
+	var cUpdate = updateCar(key_data.k);
+	enemyLoc[0] += enemyLoc[0];
+	enemyLoc[1] += enemyLoc[1];
+}
+/*
 function newDrawing(data) {
 	fill(0, 0, 0);
-	//console.log(data);
 	ellipse(data.x, data.y, 10, 10);
 }
 
+
 function displayText(key_data) {
-	//console.log(key_data);
 	text(key_data.k, screen_dims[0]*random(), screen_dims[1]*random());
 }
 
-function draw() {
-	imageMode(CENTER);
-	fill(255, 255, 255);
-	image(picture, imageLoc[0], imageLoc[1]);
-	//image(picture, mouseX, mouseY);
-}
-
-
-function displayCar()
+function mousePressed()
 {
-	//background(255, 255, 255);
-	imageMode(CENTER);
-	fill(255, 255, 255);
-	image(picture, imageLoc[0], imageLoc[1]);
-}
-
-
-function updateCar(key)
-{
-	var speed = 10;
-	if (key == 'A' || key == 'a')
-	{
-		imageLoc[0] -= speed;
-	}
-	if (key == 'd' || key == 'D')
-	{
-		imageLoc[0] += speed;
-	}
-	if (key == 'w' || key == 'W')
-	{
-		imageLoc[1] -= speed;
-	}
-	if (key == 's' || key == 'S')
-	{
-		imageLoc[1] += speed;
-	}
-}
-
-function mousePressed() {
-	//console.log(mouseX, mouseY);
 	var data = {
 		x: mouseX,
 		y: mouseY
@@ -78,13 +96,4 @@ function mousePressed() {
 	socket.emit('mouse', data);
 	newDrawing(data);
 }
-
-function keyReleased() {
-	//console.log(key);
-	updateCar(key);
-	var key_data = {
-		k: key
-	}
-	socket.emit('key', key_data);
-	displayText(key_data);
-}
+*/
